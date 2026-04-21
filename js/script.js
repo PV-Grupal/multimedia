45015cd476113c19a6889ce6f4bb8f17860792b0
PokeSantiTW <santibicho@gmail.com>
2026-04-21 14:30:58 +0200

    Implementación Disqus

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log("test");
+
+
+// Implementación de Disqus. NO EDITAR.
+
+/**
+*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
+
+var disqus_config = function () {
+this.page.url = "https://pv-grupal.github.io/multimedia/";  // Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = "SILVER_STRIDER"; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = 'https://silver-strider.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
